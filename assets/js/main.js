d9ac8c7aee10f21876130b33076f99605c85877d
Seth Akkerman <sethakkerman@gmail.com>
2016-03-19 15:53:52 -0400

    Slight edits to Coming Soon Page

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,18 @@
 (function( window, $, undefined ){
 	"use strict";
 	if (navigator.userAgent.match(/(iPad)|(iPhone)|(iPod)|(android)|(webOS)/i)){
-		// only put things here for touch devices	
+		// only put things here for touch devices
+		/*$(function() {
+			$("img").bind('touchstart', function() {
+				var src = $(this).attr("src").replace(".png", "_o.png");
+				$(this).attr("src", src);
+			});
+	
+			$("img").bind('touchend', function() {
+				var src = $(this).attr("src").replace("_o.png", ".png");
+				$(this).attr("src", src);
+			});
+		});*/
 	} else {
 		//seth quick hover swap start
 		$(function() {
