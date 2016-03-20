3fabdea832706d414365283ddcb505feb199a05c
Seth Akkerman <sethakkerman@gmail.com>
2016-03-20 14:05:32 -0400

    Changed PS Labs from a Coming soon to v1.0

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,12 +3,12 @@
 	if (navigator.userAgent.match(/(iPad)|(iPhone)|(iPod)|(android)|(webOS)/i)){
 		// only put things here for touch devices
 		/*$(function() {
-			$("img").bind('touchstart', function() {
+			$("img.logo").bind('touchstart', function() {
 				var src = $(this).attr("src").replace(".png", "_o.png");
 				$(this).attr("src", src);
 			});
 	
-			$("img").bind('touchend', function() {
+			$("img.logo").bind('touchend', function() {
 				var src = $(this).attr("src").replace("_o.png", ".png");
 				$(this).attr("src", src);
 			});
@@ -16,7 +16,7 @@
 	} else {
 		//seth quick hover swap start
 		$(function() {
-			$("img")
+			$("img.logo")
 			.mouseover(function() { 
 				var src = $(this).attr("src").replace(".png", "_o.png");
 				$(this).attr("src", src);
